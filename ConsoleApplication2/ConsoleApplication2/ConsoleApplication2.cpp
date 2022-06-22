6d2e9fa22fbf9da9099d403df1a2f18eef818c68
RomanNovikov48 <roman_novikov_02@list.ru>
2022-06-22 18:08:08 +0300

    Practice1

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int number, count;
+
+	cout << "Введите число: ";
+	cin >> number;
+
+	count = number;
+
+	while (number != 0)
+	{
+		cout << count << " цифра: " << number % 10 << endl;
+
+		count--;
+
+		number /= 10;
+	}
+
+	return 0;
+}
